116accbe385a338f5ff485c5d446acc66fd965f6
DESKTOP-NQO1TN8\David <daviddcastro03@gmail.com>
2025-02-14 20:17:16 +0100

    agregar datos de Pedro al ts y al html

--- proyecto001/src/app/app.component.ts
+++ proyecto001/src/app/app.component.ts
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'proyecto001';
+  title : string = 'proyecto001';
+  nombre = 'Pedro';
+  edad = 21;
+  fumador = false;
+  sueldo : number[] = [1200, 1500, 1700];
+
+  esFumador() {
+    return this.fumador?"Es fumador":"No es fumador";
+  }
 }
+
+
